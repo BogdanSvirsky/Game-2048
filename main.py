8f00e2674e07daaef76790eed80e881951298f9c
BogdanSvirsky <svirskybogdan@yandex.ru>
2021-02-23 14:58:36 +0700

    Постепенно идёт работа

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import pygame
+import pygame_gui
 from board import Board
+
+
+def draw_screen(screnn):
+    board.render()
+    font = pygame.font.Font(None, 50)
+    text_score = font.render("Счёт", True, (100, 50, 0))
+    text_x = 50
+    text_y = 30
+    text_w = text_score.get_width()
+    text_h = text_score.get_height()
+    screen.blit(text_score, (text_x, text_y))
+    pygame.draw.rect(screen, (180, 130, 80), (text_x - 10, text_y + 50,
+                                              text_w + 20, text_h + 20), 0, border_radius=5)
+    text_score_value = font.render(str(sum([sum(x) for x in board.board])), True, (100, 50, 0))
+    screen.blit(text_score_value, (40 + ((text_w + 20 - text_score_value.get_width()) // 2),
+                                   81 + ((text_h + 20 - text_score_value.get_height()) // 2)))
+    pygame.display.flip()
 
 
 pygame.init()
@@ -16,7 +34,5 @@ while running:
             print(event.pos)
         if event.type == pygame.KEYDOWN:
             board.move(event.type)
-    screen.fill((239, 220, 214))
-    board.render()
-    pygame.display.flip()
+    draw_screen(screen)
 pygame.quit()
